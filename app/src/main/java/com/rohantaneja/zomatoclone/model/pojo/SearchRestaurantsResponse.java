e53875b1e0b633f6ce729cae847fce658a4522cf
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-04 16:52:31 +0530

    added header to retrofitAdapter

---
+++ app/src/main/java/com/rohantaneja/zomatoclone/model/pojo/SearchRestaurantsResponse.java
@@ -0,0 +1,8 @@
+package com.rohantaneja.zomatoclone.model.pojo;
+
+/**
+ * Created by rohantaneja on 04/04/18.
+ */
+
+public class SearchRestaurantsResponse {
+}
--- app/src/main/java/com/rohantaneja/zomatoclone/network/RetrofitAdapter.java
+++ app/src/main/java/com/rohantaneja/zomatoclone/network/RetrofitAdapter.java
@@ -1,5 +1,11 @@
 package com.rohantaneja.zomatoclone.network;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,8 +17,20 @@ public class RetrofitAdapter {
     private ZomatoAPI zomatoAPI;
 
     public RetrofitAdapter(String baseUrl) {
+        OkHttpClient.Builder okHttpBuilder = new OkHttpClient.Builder();
+        okHttpBuilder.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request().newBuilder().addHeader("user-key", "c750173e8cf7e5fdc2c331cf897ee8c3").build();
+                return chain.proceed(request);
+            }
+        });
+
+        OkHttpClient okHttpClient = okHttpBuilder.build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
