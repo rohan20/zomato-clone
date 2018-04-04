e4ab23ca715974a25be7201cc43d4da496b5f97a
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-04 17:51:22 +0530

    setup search edit text visibility

--- app/src/main/java/com/rohantaneja/zomatoclone/ui/RestaurantsActivity.java
+++ app/src/main/java/com/rohantaneja/zomatoclone/ui/RestaurantsActivity.java
@@ -3,10 +3,15 @@ package com.rohantaneja.zomatoclone.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rohantaneja.zomatoclone.R;
 import com.rohantaneja.zomatoclone.model.pojo.RestaurantWrapper;
@@ -23,13 +28,15 @@ import retrofit2.Response;
 public class RestaurantsActivity extends AppCompatActivity {
 
     private static final String TAG = "RestaurantsActivity";
-
     private EditText searchRestaurantEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+        searchRestaurantEditText = findViewById(R.id.search_edit_text);
+
     }
 
     @Override
@@ -47,21 +54,42 @@ public class RestaurantsActivity extends AppCompatActivity {
                 break;
 
             case R.id.action_search:
-                searchRestaurants();
+                showSearchBar();
+//                searchRestaurants();
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void showSearchBar() {
+
+        searchRestaurantEditText.setVisibility(View.VISIBLE);
+
+        searchRestaurantEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+
+                    String searchQuery = searchRestaurantEditText.getText().toString();
+
+                    if (!searchQuery.isEmpty())
+                        searchRestaurants(searchQuery);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
     private void filterRestaurantResults() {
     }
 
-    private void searchRestaurants() {
+    private void searchRestaurants(String searchQuery) {
 
         Log.d(TAG, "Searching...");
 
         RetrofitAdapter retrofitAdapter = new RetrofitAdapter(Constants.RESTAURANTS_BASE_URL);
-        Call<SearchRestaurantsResponse> restaurantsResponse = retrofitAdapter.getZomatoAPI().getRestaurantsResponse("saket", 1, 20);
+        Call<SearchRestaurantsResponse> restaurantsResponse = retrofitAdapter.getZomatoAPI().getRestaurantsResponse(searchQuery, 1, 20);
 
         restaurantsResponse.enqueue(new Callback<SearchRestaurantsResponse>() {
             @Override
@@ -81,6 +109,12 @@ public class RestaurantsActivity extends AppCompatActivity {
     }
 
     private void populateRestaurantsList(List<RestaurantWrapper> restaurants) {
+
+        if (restaurants.size() == 0) {
+            Toast.makeText(this, "No results found", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         Log.d(TAG, restaurants.get(0).getRestaurant().getName());
         Log.d(TAG, restaurants.get(1).getRestaurant().getName());
         Log.d(TAG, restaurants.get(2).getRestaurant().getName());
