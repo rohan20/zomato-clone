e8442a65d95cbedcd538d5fe0544e3ac15e91744
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-04 17:15:11 +0530

    added filter and search menu items

--- app/src/main/java/com/rohantaneja/zomatoclone/ui/RestaurantsActivity.java
+++ app/src/main/java/com/rohantaneja/zomatoclone/ui/RestaurantsActivity.java
@@ -2,6 +2,9 @@ package com.rohantaneja.zomatoclone.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.rohantaneja.zomatoclone.R;
 
@@ -11,5 +14,32 @@ public class RestaurantsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_filer:
+                filterRestaurantResults();
+                break;
+
+            case R.id.action_search:
+                searchRestaurants();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void filterRestaurantResults() {
+    }
+
+    private void searchRestaurants() {
     }
 }
