779537b2339e4395fb03f71a73c91f956045848e
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-04 17:45:21 +0530

    setup API query

--- app/src/main/java/com/rohantaneja/zomatoclone/network/ZomatoAPI.java
+++ app/src/main/java/com/rohantaneja/zomatoclone/network/ZomatoAPI.java
@@ -2,8 +2,8 @@ package com.rohantaneja.zomatoclone.network;
 
 import com.rohantaneja.zomatoclone.model.pojo.SearchRestaurantsResponse;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 /**
@@ -13,7 +13,7 @@ import retrofit2.http.Query;
 public interface ZomatoAPI {
 
     @GET("search")
-    SearchRestaurantsResponse getRestaurantsResponse(
+    Call<SearchRestaurantsResponse> getRestaurantsResponse(
             @Query("q") String query,
             @Query("start") int start,
             @Query("count") int count
--- app/src/main/java/com/rohantaneja/zomatoclone/ui/RestaurantsActivity.java
+++ app/src/main/java/com/rohantaneja/zomatoclone/ui/RestaurantsActivity.java
@@ -2,13 +2,29 @@ package com.rohantaneja.zomatoclone.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.rohantaneja.zomatoclone.R;
+import com.rohantaneja.zomatoclone.model.pojo.RestaurantWrapper;
+import com.rohantaneja.zomatoclone.model.pojo.SearchRestaurantsResponse;
+import com.rohantaneja.zomatoclone.network.RetrofitAdapter;
+import com.rohantaneja.zomatoclone.util.Constants;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
+
+    private static final String TAG = "RestaurantsActivity";
+
+    private EditText searchRestaurantEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +41,7 @@ public class RestaurantsActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_filer:
                 filterRestaurantResults();
                 break;
@@ -41,5 +57,32 @@ public class RestaurantsActivity extends AppCompatActivity {
     }
 
     private void searchRestaurants() {
+
+        Log.d(TAG, "Searching...");
+
+        RetrofitAdapter retrofitAdapter = new RetrofitAdapter(Constants.RESTAURANTS_BASE_URL);
+        Call<SearchRestaurantsResponse> restaurantsResponse = retrofitAdapter.getZomatoAPI().getRestaurantsResponse("saket", 1, 20);
+
+        restaurantsResponse.enqueue(new Callback<SearchRestaurantsResponse>() {
+            @Override
+            public void onResponse(Call<SearchRestaurantsResponse> call, Response<SearchRestaurantsResponse> response) {
+                if (response.isSuccessful()) {
+                    populateRestaurantsList(response.body().getRestaurants());
+                } else {
+                    Log.d(TAG, "Response Unsuccessful");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SearchRestaurantsResponse> call, Throwable t) {
+                Log.d(TAG, "onFail: " + t.getLocalizedMessage());
+            }
+        });
+    }
+
+    private void populateRestaurantsList(List<RestaurantWrapper> restaurants) {
+        Log.d(TAG, restaurants.get(0).getRestaurant().getName());
+        Log.d(TAG, restaurants.get(1).getRestaurant().getName());
+        Log.d(TAG, restaurants.get(2).getRestaurant().getName());
     }
 }
